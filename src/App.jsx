fc3187e77030d3be4be3637505fc2b35cf44bf8b
erfajc97 <erfacrypto@gmail.com>
2024-12-13 17:53:15 -0300

    [ADD] first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold underline mb-4">Hola mundo</p>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        Click me
+      </button>
+    </>
+  );
+}
+
+export default App;
